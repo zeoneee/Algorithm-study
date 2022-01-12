b01380799223f12758e0825617bf327d115401b3
Jihyeon Kim <jiwlgus048@gmail.com>
2022-01-12 16:17:32 +0900

    0112

---
+++ 유형별/그리디/단어수학.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline())
+string = []
+
+# 단어 입력받기
+for _ in range(n):
+    string.append(input())
+
+string_sorted = sorted(string,key=lambda x:len(x),reverse=True)
+
+# 길이 체크 -> 맞게 출력됨 
+for i in string_sorted:
+    print(i)
