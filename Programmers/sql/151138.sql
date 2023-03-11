9a421e66c5cbb8eeca4f84e07ec4bcfacd718cc5
jihyeon <jiwlgus048@gmail.com>
2023-03-11 19:17:51 +0900

    String, Date 자동차 대여 기록에서 장기/단기 대여 구분하기

---
+++ Programmers/sql/151138.sql
@@ -0,0 +1,21 @@
+-- 코드를 입력하세요
+SELECT 
+HISTORY_ID, CAR_ID,
+DATE_FORMAT(START_DATE, '%Y-%m-%d') AS START_DATE
+,DATE_FORMAT(END_DATE, '%Y-%m-%d') AS END_DATE,
+CASE
+    WHEN DATEDIFF(END_DATE, START_DATE) >= 29   # 빌린날짜 포함
+    THEN "장기 대여"
+    ELSE "단기 대여"
+END AS RENT_TYPE
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY 
+WHERE
+    START_DATE BETWEEN '2022-09-01' AND '2022-09-30'
+ORDER BY 
+  HISTORY_ID DESC;
+
+/*
+    CASE END AS문: switch문과 비슷. 조건 설정해서 select 혹은 함수 적용
+    ORDER BY DESC: 내림차순
+    DATE_FORMAT(DATE, ''): 데이터 포맷 설정
+*/
