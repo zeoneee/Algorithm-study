02efdf9548fc05f2882559286331a3f1b5e716ff
jihyeon <jiwlgus048@gmail.com>
2023-05-06 21:54:56 +0900

    JadenCase 문자열 만들기 solved

--- Programmers/12951.py
+++ Programmers/12951.py
@@ -1,15 +1,15 @@
 def solution(s):
     answer = ''
-    jadencase = s.split()
+    jadencase = s.split(" ")
+    print(jadencase)
     for j in jadencase:
-        if j[0].isdigit():
+        if j == '':
+            answer += " "
+        elif j[0].isdigit():
             answer += j.lower() + ' '
-        elif j[0] == ' ':
-            answer += j[0]
         else:
             temp = j[0].upper()
             temp += j[1:].lower()
             answer += temp + ' '
     
     return answer[:-1] # 마지막 띄어쓰기 제외
-    
