fb49af196f081a3f316c7665a810437c7d61e360
zeoneee <jiwlgus048@gmail.com>
2023-05-13 22:35:43 +0900

    마법의 엘리베이터 unsolved 46.2/100

--- Programmers/148653.py
+++ Programmers/148653.py
@@ -1,8 +1,22 @@
 def solution(storey):
     answer = 0
     storey = list(map(int,str(storey)))
-    if storey[-1] >= 5:
-        answer = sum(storey[:-1]) + 1 + 10-storey[-1]
-    else:
-        answer = sum(storey)
+    for i,s in enumerate(storey):
+        if s > 5:
+            if i != len(storey)-1:
+                if storey[i+1] >5:
+                    answer += 10-s-1
+                else:
+                    answer += 10-s
+            else:
+                answer += 10-s
+        else:
+            if i != len(storey)-1:
+                if storey[i+1] >5:
+                    answer += s+1
+                else:
+                    answer += s
+            else:
+                answer += s
+                
     return answer
