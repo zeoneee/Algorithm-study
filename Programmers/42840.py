8cce68899c99de950ae9a1f84df864cea04f1d4a
zeoneee <jiwlgus048@gmail.com>
2023-05-19 22:51:41 +0900

    모의고사 solved

--- Programmers/42840.py
+++ Programmers/42840.py
@@ -1,29 +1,19 @@
 def solution(answers):
-    cnt = [[0,1],[0,2],[0,3]]
+    answer = []
+    score = [0,0,0]
     submit = [[1,2,3,4,5],[2,1,2,3,2,4,2,5],[3,3,1,1,2,2,4,4,5,5]]
     
-    # answers 길이 이상만큼 제출한답 만들어주기
-    for i in range(3):
-        temp_l = len(submit[0])
-        submit[i] = submit[i]*(len(answers)//temp_l)
-        for j in range((len(answers)%temp_l)):
-            submit[i] + submit[j]
-    
-    # 답 검사하기
     for i,v in enumerate(answers):
-        for j in range(3):
-            if v == submit[j][i]:
-                cnt[j][0] += 1   
-    
-    # max 값 찾기
-    cnt.sort(reverse=True)
-    answer = [cnt[0][1]]
-    
-    if cnt[0][0] == cnt[1][0]:
-        answer.append(cnt[1][1])
-        if cnt[1][0] == cnt[2][0]:
-            answer.append(cnt[2][1])
+        if submit[0][i%5] == v:
+            score[0] += 1
+        if submit[1][i%8] == v:
+            score[1] += 1
+        if submit[2][i%10] == v:
+            score[2] += 1
             
-    answer.sort()   
-    
+            
+    for i in range(len(score)):
+        if max(score) == score[i]:
+            answer.append(i+1)
+            
     return answer
