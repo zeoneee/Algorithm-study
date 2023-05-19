3a331bc4785b2527a06b1a2f983e4229ef3e6202
zeoneee <jiwlgus048@gmail.com>
2023-05-19 21:32:35 +0900

    마법의 엘리베이터 solved

--- Programmers/148653.py
+++ Programmers/148653.py
@@ -1,22 +1,25 @@
 def solution(storey):
     answer = 0
-    storey = list(map(int,str(storey)))
-    for i,s in enumerate(storey):
-        if s > 5:
-            if i != len(storey)-1:
-                if storey[i+1] >5:
-                    answer += 10-s-1
-                else:
-                    answer += 10-s
+    nums = list(map(int, list(str(storey))))
+    nums = [[n,i] for i,n in enumerate(nums)]
+    
+    while nums:
+        n,i = nums.pop()
+        if n < 5:
+            answer += n
+        elif n > 5:
+            answer += 10-n
+            if i == 0:
+                nums.append((1,0)) # 맨 앞자리일 때 오름1 있다면
             else:
-                answer += 10-s
+                nums[i-1][0] += 1
         else:
-            if i != len(storey)-1:
-                if storey[i+1] >5:
-                    answer += s+1
-                else:
-                    answer += s
+            if i == 0 or nums[i-1][0] < 5: # 맨 앞자리 일때
+                answer += n 
             else:
-                answer += s
+                answer += 10-n
+                nums[i-1][0] += 1
                 
     return answer
+
+# data를 stack화해서 하나씩 pop()하면서 뒷자리부터 검사하는 아이디어 
