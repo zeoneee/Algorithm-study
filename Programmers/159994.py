36ae5032b99bbc5623f565fc991e2f9bce38988a
zeoneee <jiwlgus048@gmail.com>
2023-05-27 23:21:32 +0900

    카드뭉치 solved

---
+++ Programmers/159994.py
@@ -0,0 +1,11 @@
+def solution(cards1, cards2, goal):
+    answer = []
+    for g in goal:
+        if len(cards1) != 0 and g == cards1[0]:
+            answer.append(cards1.pop(0)) 
+        elif len(cards2) != 0 and g == cards2[0]:
+            answer.append(cards2.pop(0))
+        else:
+            return "No"
+
+    return "Yes" if answer == goal else "No"
