a8503f65ed3531f50ea8dea6b94f9122623328e1
zeoneee <jiwlgus048@gmail.com>
2023-06-21 15:29:31 +0900

    괄호 회전하기 solved (더 간단한 코드)

--- Programmers/76502.py
+++ Programmers/76502.py
@@ -1,36 +1,15 @@
 def solution(s):
-    answer = 0
-    l = len(s)
-    s *= 2
-    right = True
+    ans, pair = 0,{'{':'}', '[':']', '(':')'}
+    for i in range(len(s)):
+        iscorrect, stack = True, []
+        for v in s:
+            if v in ['{','[','(']: 
+                stack.append(v)
+            elif not stack or v != pair[stack[-1]]: 
+                iscorrect = False # stack.pop()해서 비교하는거랑 같음
+            else: stack = stack[:-1] # 비교했을때 같다면 그대로 pop
+        
+        ans += int(iscorrect and not stack) # iscorrect가 True고 stack이 true일때
+        s = s[1:]+s[0]
     
-    for i in range(l):
-        check = s[i:i+l]
-        li = []
-        
-        for j in check:
-            if j == "(" or j == "{" or j=="[":
-                li.append(j)
-            elif len(li) != 0:
-                if j == ")":
-                    if li.pop() != "(":
-                        right = False
-                        break
-                elif j == "]":
-                    if li.pop() != "[":
-                        right = False
-                        break
-                else:   
-                    if li.pop() != "{":
-                        right = False
-                        break
-            else:
-                right=False
-                        
-        if right == False:
-            right = True
-        else:
-            if len(li) == 0:
-                answer += 1
-        
-    return answer
+    return ans
