c948bc36ea47ee7cb261074796881c39885d9a78
zeoneee <jiwlgus048@gmail.com>
2023-09-07 11:01:52 +0900

    옹알이(2) solved 2차

--- Programmers/133499.py
+++ Programmers/133499.py
@@ -1,35 +1,13 @@
 def solution(babbling):
+    says = ["aya","ye","woo","ma"]
     answer = 0
+    
     for word in babbling:
-        i = 0
-        prev = ""
-        while i < len(word):
-            if word[i:i+3] == "aya":
-                if prev == "aya":
-                    answer -= 1
-                    break
-                prev = "aya"
-                i += 3
-            elif word[i:i+2] == "ye":
-                if prev == "ye":
-                    answer -= 1
-                    break
-                prev = "ye"
-                i += 2
-            elif word[i:i+3] == "woo":
-                if prev == "woo":
-                    answer -= 1
-                    break
-                prev = "woo"
-                i += 3
-            elif word[i:i+2] == "ma":
-                if prev == "ma":
-                    answer -= 1
-                    break
-                prev = "ma"
-                i += 2
-            else:
-                answer -= 1
-                break
-        answer += 1
+        for say in says:
+            if say in word and say*2 not in word: # 연속된 say 검사
+                word = word.replace(say,"*") # say은 다 *로 바꾸기
+                
+        if all(char == "*" for char in word):
+            answer += 1
+        
     return answer
