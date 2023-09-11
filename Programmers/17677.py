dac713c68d4bb98597e6b40372875be5ca20fabd
zeoneee <jiwlgus048@gmail.com>
2023-09-11 12:56:37 +0900

    [1차] 뉴스 클러스터링 solved

--- Programmers/17677.py
+++ Programmers/17677.py
@@ -1,23 +1,39 @@
-# J(A,B) = 두 집합의 교집합 크기 / 두 집합의 합집합 크기
-import re
+# 유사도: 교집합 / 합집합
+# 다중집합 개념 사용
 
-def solution(str1, str2):    
+def solution(str1, str2):
     answer = 0
+    s1 = []
+    s2 = []
     
-    # 다중집합 만들기
-    s1, s2 = [], []
     for i in range(len(str1)-1):
-        if str1[i].isalpha() and str1[i+1].isalpha():
-            s1.append(str1[i].upper()+str1[i+1].upper())
+        if str1[i:i+2].isalpha():
+            s1.append(str1[i:i+2].upper())
+            
     for i in range(len(str2)-1):
-        if str2[i].isalpha() and str2[i+1].isalpha():
-            s2.append(str2[i].upper()+str2[i+1].upper())   
-        
-    print(s1,s2)
+        if str2[i:i+2].isalpha():
+            s2.append(str2[i:i+2].upper())
+            
+    # 다중집합의 합집합
+    s1_temp = s1.copy()
+    s1_result = s1.copy() # 합집합
     
-    if len(set(s1)&set(s2)) == 0 or len(set(s1+s2)) == 0:
-        return 65536
-    answer = (len(set(s1)&set(s2))/len(set(s1+s2))) * 65536
-    return answer
-
-# 같은 문자도 다르게 판단해야하나봄 그럼 set을 사용하면 안되는데
+    for s in s2:
+        if s in s1_temp:
+            s1_temp.remove(s)
+        else:
+            s1_result.append(s)
+    
+    # 다중집합의 교집합
+    s1_result2 = []
+    for s in s2:
+        if s in s1:
+            s1.remove(s)
+            s1_result2.append(s)
+        
+    if len(s1_result) == 0 and len(s1_result2) == 0:
+        answer = 1
+    else:
+        answer = len(s1_result2)/len(s1_result)
+        
+    return int(answer*65536)
