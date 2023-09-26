48cc7625efded51f74e8690c5acb22e3f37e1670
zeoneee <jiwlgus048@gmail.com>
2023-09-26 15:35:59 +0900

    땅따먹기 Lv2 dp solved

--- Programmers/12913.py
+++ Programmers/12913.py
@@ -1,20 +1,9 @@
-#내 행과 다음 행을 비교했을 때 
 def solution(land):
     answer = 0
+
+    for i in range(1,len(land)):
+        for j in range(4):
+            land[i][j] += max(land[i-1][:j]+land[i-1][j+1:])
     
-    for i in range(len(land)):
-        if i+1 == len(land):
-            answer += max(land[i])
-        elif land[i].index(max(land[i])) != land[i+1].index(max(land[i+1])):
-            answer += max(land[i])
-        else:
-            temp_i = land[i].copy()
-            temp_i.remove(max(land[i]))
-            temp_i1 = land[i+1].copy()
-            temp_i1.remove(max(land[i+1]))
-            if max(land[i]) + max(temp_i1) > max(temp_i) + max(land[i+1]):
-                answer += max(land[i])
-                land[i+1] = temp_i1
-            else:
-                answer += max(temp_i)
+    answer = max(land[len(land)-1])
     return answer
