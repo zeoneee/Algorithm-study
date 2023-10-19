e46d73e29e4cebd415977ad02e4c2b4a5e838d84
zeoneee <jiwlgus048@gmail.com>
2023-10-19 20:15:25 +0900

    네트워크 L3 플로이드와샬 solved

--- Programmers/43162.py
+++ Programmers/43162.py
@@ -1,22 +1,20 @@
-def dfs(visited, v, graph):
-    if visited[v] == False:
-        visited[v] = True
-        print(v,end = ' ')
-        network = unleafnode = 0
-        for idx,n in enumerate(graph[v]):
-            if n == 1 and idx != v and visited[idx] == False:
-                unleafnode = 1
-                network += dfs(visited, idx, graph)
-        return network if unleafnode == 1 else 1  # leafnode인 경우 return 1 
-    return 0 # 이미 이전에 방문했던 노드이므로 pass
-
-
 def solution(n, computers):
     answer = 0
-    visited = [False]*n
+     
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if computers[i][k] == 1 and computers[k][j] == 1:
+                    computers[i][j] = 1
+                    computers[j][i] = 1
     
-    for i in range(n):
-        if (dfs(visited,i,computers) != 0):
+    visited = [False]*(n+1)
+    for i,computer in enumerate(computers):
+        if visited[i] == False:
             answer += 1
+            visited[i] = True
+            for i,v in enumerate(computer):
+                if v == 1:
+                    visited[i] = True
     
     return answer
